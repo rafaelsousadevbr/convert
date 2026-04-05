c181abd4aa0461c61d2e93beeef779256eb3799a
rafaelsousadevbr <rafael@sousalima.dev.br>
2026-04-05 12:10:53 -0300

    fix: handle footer visibility and error alert in convertCurrency function

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const GBP = 6.83;
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
 
 amount.addEventListener("input", () => {
     const hasCharactersRegex = /[^\d,]/g;
@@ -29,4 +30,10 @@ form.onsubmit = (event) => {
 };
 
 function convertCurrency(amount, price, symbol) {
+    try {
+        footer.classList.add("show-result");
+    } catch (error) {
+        footer.classList.remove("show-result");
+        alert("Ocorreu um erro ao tentar converter eexibir o resultado. Por favor, tente novamente.");
+    }
 }
