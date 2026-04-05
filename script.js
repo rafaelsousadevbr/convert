3ca47bd9d86d392426002aace8c0c542424207bd
rafaelsousadevbr <rafael@sousalima.dev.br>
2026-04-04 23:19:30 -0300

    refactor: reorganize currency constants and enhance form submission handling and create the convertCurrency function

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const form = document.querySelector('form');
-const amount = document.getElementById('amount');
-const currency = document.getElementById('currency');
+//Cotações de moedas no dia
+const USD = 5.16;
+const EUR = 5.96;
+const GBP = 6.83;
 
-amount.addEventListener('input', () => {
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+
+amount.addEventListener("input", () => {
     const hasCharactersRegex = /[^\d,]/g;
-    amount.value = amount.value.replace(hasCharactersRegex, '');
+    amount.value = amount.value.replace(hasCharactersRegex, "");
 });
 
 form.onsubmit = (event) => {
     event.preventDefault();
 
-    console.log(amount.value, currency.value);
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "US$");
+            break;
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€");
+            break;
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£");
+            break;
+    }
 };
+
+function convertCurrency(amount, price, symbol) {
+}
