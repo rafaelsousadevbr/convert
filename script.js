e97e6b67fad9c84f39c30438a37616760f00264c
rafaelsousadevbr <rafael@sousalima.dev.br>
2026-04-05 23:11:53 -0300

    fix: update currency conversion logic and enhance result display

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 amount.addEventListener("input", () => {
     const hasCharactersRegex = /[^\d,]/g;
@@ -32,8 +34,17 @@ form.onsubmit = (event) => {
 function convertCurrency(amount, price, symbol) {
     try {
         footer.classList.add("show-result");
+        description.textContent = `${symbol} 1 = R$ ${formatCurrency(price)}`;
+        result.textContent = formatCurrency(amount * price);
     } catch (error) {
         footer.classList.remove("show-result");
         alert("Ocorreu um erro ao tentar converter eexibir o resultado. Por favor, tente novamente.");
     }
+}
+
+function formatCurrency(value) {
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
 }
